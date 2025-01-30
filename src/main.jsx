712a8318c3d1044e234e8bb7ca3adfb9b1571287
Bhagwat Munde <123530941+Mundebhagwat@users.noreply.github.com>
2025-01-30 16:01:52 +0530

    Sign in with Google Updated (#6)
    * name change
    
    * Sign in with google Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import './index.css'
-import App from './App.jsx'
-import { CssBaseline } from '@mui/material'
-import React from 'react'
-import Notification from './notification/Notification.jsx'
-import useAuthStore from './utils/useAuthStore.js'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { ThemeProvider, createTheme } from '@mui/material/styles'
+// import './index.css'
+// import App from './App.jsx'
+// import { CssBaseline } from '@mui/material'
+// import React from 'react'
+// import Notification from './notification/Notification.jsx'
+// import useAuthStore from './utils/useAuthStore.js'
 
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<CssBaseline />
-		<App />
-		<Notification />
-	</StrictMode>
+// createRoot(document.getElementById('root')).render(
+// 	<StrictMode>
+// 		<CssBaseline />
+// 		<App />
+// 		<Notification />
+// 	</StrictMode>
+// )
 
-	// Bhagwat
-)
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./index.css";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
+import React from "react";
+import Notification from "./notification/Notification.jsx";
+import useAuthStore from "./utils/useAuthStore.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID = "977070351773-06bn9pmaa4apuqjdhnijiek93kqj41jq.apps.googleusercontent.com"; // ✅ Fixed Client ID (Removed "http://")
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}> {/* ✅ Wrap entire app */}
+            <CssBaseline />
+            <App />
+            <Notification />
+        </GoogleOAuthProvider>
+    </StrictMode>
+);
+
 
 
 
